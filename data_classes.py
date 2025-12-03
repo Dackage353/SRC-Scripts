891c65c3da8e2076f0d15f3b6df52d1be0adcc3f
Daniel <daniel.g.hackel@gmail.com>
2025-12-03 00:32:06 -0600

    style work

--- data_classes.py
+++ data_classes.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
 
+
 class Run:
     def __init__(self, run_id, game_id, category_id, player_id, time, run_date, time_submitted, time_verified, verifier_id, verified_status):
         self.run_id = run_id
@@ -16,17 +17,18 @@ class Run:
     @staticmethod
     def create_from_json(run_data):
         return Run(
-            run_id             = run_data['id'],
-            game_id            = run_data['game'],
-            category_id        = run_data['category'],
-            player_id          = run_data['players'][0]['id'],
-            time               = run_data['times']['primary_t'],
-            run_date           = datetime.strptime(run_data['date'], "%Y-%m-%d"),
-            time_submitted     = datetime.strptime(run_data['submitted'], "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc),
-            time_verified      = run_data['status'].get('verify-date'),
-            verifier_id        = run_data['status']['examiner'],
-            verified_status    = run_data['status']['status']
+            run_id             = run_data["id"],
+            game_id            = run_data["game"],
+            category_id        = run_data["category"],
+            player_id          = run_data["players"][0]["id"],
+            time               = run_data["times"]["primary_t"],
+            run_date           = datetime.strptime(run_data["date"], "%Y-%m-%d"),
+            time_submitted     = datetime.strptime(run_data["submitted"], "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc),
+            time_verified      = run_data["status"].get("verify-date"),
+            verifier_id        = run_data["status"]["examiner"],
+            verified_status    = run_data["status"]["status"]
         )
+        
         
 class Game:
     def __init__(game_id, name, abbreviation, release_date, platform_ids, moderator_ids, level_ids):
@@ -38,12 +40,14 @@ class Game:
         self.moderator_ids = moderator_ids
         self.level_ids = level_ids
         
+        
 class Category:
     def __init__(category_id, name, link, run_ids):
         self.category_id = category_id
         self.name = name
         self.link = link
         self.run_ids = run_ids
+        
         
 class Level:
     def __init__(level_id, name, link):
--- src_helper.py
+++ src_helper.py
@@ -2,96 +2,96 @@ import time
 import requests
 
 _HEADERS = {
-    'User-Agent': 'src-stats/1.0',
-    'Accept': 'application/json',
+    "User-Agent": "src-stats/1.0",
+    "Accept": "application/json",
 }
 _h = dict(_HEADERS)
 _sleep_period = .75
 _retry_sleep = 30
 
-NON_SRC_USERS = ['Maha Maha', 'Cryogeon', 'Madghostek', 'WWMResident','atmpas','CeeSZee','255']
+NON_SRC_USERS = ["Maha Maha", "Cryogeon", "Madghostek", "WWMResident","atmpas","CeeSZee","255"]
 
 
 # url help
 def get_runs_url():
-    return 'https://www.speedrun.com/api/v1/runs'
+    return "https://www.speedrun.com/api/v1/runs"
 
 
 def get_runs_for_game_category(game_id, category):
-    return f'https://www.speedrun.com/api/v1/runs?game={game_id}&category={category}&status=verified&orderby=verify' \
-           f'-date&direction=desc&embed=players'
+    return f"https://www.speedrun.com/api/v1/runs?game={game_id}&category={category}&status=verified&orderby=verify" \
+           f"-date&direction=desc&embed=players"
 
 
 def get_leaderboard_for_game_category(game_id, category):
-    return f'https://www.speedrun.com/api/v1/leaderboards/{game_id}/category/{category}?embed=players'
+    return f"https://www.speedrun.com/api/v1/leaderboards/{game_id}/category/{category}?embed=players"
 
 
 def get_leaderboard_for_game_level_category(game_id, level_id,category):
-    return f'https://www.speedrun.com/api/v1/leaderboards/{game_id}/level/{level_id}/{category}?embed=players'
+    return f"https://www.speedrun.com/api/v1/leaderboards/{game_id}/level/{level_id}/{category}?embed=players"
 
 
 def get_wr_for_game_category(game_id, category):
-    return f'https://www.speedrun.com/api/v1/leaderboards/{game_id}/category/{category}?top=1&embed=players'
+    return f"https://www.speedrun.com/api/v1/leaderboards/{game_id}/category/{category}?top=1&embed=players"
 
 
 def get_runs_for_game(game_id):
-    return f'https://www.speedrun.com/api/v1/runs?game={game_id}&status=verified&orderby=verify-date&direction=desc'
+    return f"https://www.speedrun.com/api/v1/runs?game={game_id}&status=verified&orderby=verify-date&direction=desc"
 
 
 def get_romhacks():
-    return f'https://www.speedrun.com/api/v1/series/0499o64v/games?max=200&embed=categories,variables,levels'
+    return f"https://www.speedrun.com/api/v1/series/0499o64v/games?max=200&embed=categories,variables,levels"
 
 
 def get_user(user_id):
-    return f'https://www.speedrun.com/api/v1/users/{user_id}'
+    return f"https://www.speedrun.com/api/v1/users/{user_id}"
 
 
 def get_game(game_id):
-    return f'https://www.speedrun.com/api/v1/games/{game_id}'
+    return f"https://www.speedrun.com/api/v1/games/{game_id}"
 
 
 def get_category(category_id):
-    return f'https://www.speedrun.com/api/v1/categories/{category_id}'
+    return f"https://www.speedrun.com/api/v1/categories/{category_id}"
 
 
 def post_run():
-    return f'https://www.speedrun.com/api/v1/runs/'
+    return f"https://www.speedrun.com/api/v1/runs/"
 
 
 def delete_run(run_id):
-    return f'https://www.speedrun.com/api/v1/runs/{run_id}'
+    return f"https://www.speedrun.com/api/v1/runs/{run_id}"
 
 
 def get_level(level_id):
-    return f'https://www.speedrun.com/api/v1/levels/{level_id}'
+    return f"https://www.speedrun.com/api/v1/levels/{level_id}"
 
 
 def unverify_run(run_id):
-    return f'https://www.speedrun.com/api/v1/runs/{run_id}/status'
+    return f"https://www.speedrun.com/api/v1/runs/{run_id}/status"
 
 
 def get_game_variables(game_id):
-    return f'https://www.speedrun.com/api/v1/games/{game_id}/variables'
+    return f"https://www.speedrun.com/api/v1/games/{game_id}/variables"
 
 
 def get_game_id(game):
-    return f'https://www.speedrun.com/api/v1/games?name={game}'
+    return f"https://www.speedrun.com/api/v1/games?name={game}"
 
 
 def get_game_categories(game_id):
-    return f'https://www.speedrun.com/api/v1/games/{game_id}/categories'
+    return f"https://www.speedrun.com/api/v1/games/{game_id}/categories"
 
 
 def get_runs_by_user_game_cat(user_id, game_id, cat_id):
-    return f'https://www.speedrun.com/api/v1/runs?user={user_id}&game={game_id}&category={cat_id}&max=200'
+    return f"https://www.speedrun.com/api/v1/runs?user={user_id}&game={game_id}&category={cat_id}&max=200"
 
 
 def get_games_and_categories():
-    return 'https://www.speedrun.com/api/v1/games?embed=categories&max=200'
+    return "https://www.speedrun.com/api/v1/games?embed=categories&max=200"
 
 
 def get_fullgame_run_body(user_id, run_time, date, platform_id, emulated, video_link, category_id):
-    return {'run': {
+    return {"run": {
         "category": category_id,
         "date": date,
         "platform": platform_id,
@@ -111,7 +111,7 @@ def get_fullgame_run_body(user_id, run_time, date, platform_id, emulated, video_
 
 def get_fullgame_variable_run_body(user_id, run_time, date, platform_id, emulated, video_link, var_id, var_val_id,
                                    category_id):
-    return {'run': {
+    return {"run": {
         "category": category_id,
         "date": date,
         "platform": platform_id,
@@ -137,13 +137,13 @@ def get_fullgame_variable_run_body(user_id, run_time, date, platform_id, emulate
 
 def get_singlestar_run_body(user_id, category_id, run_time, date, platform_id, emulated, video_link, level_id, var_id,
                             star_id, stupid_vars):
-    rel = 'user'
+    rel = "user"
     id_param = "id"
     # probably do something less dumb
     if user_id in NON_SRC_USERS:
-        rel = 'guest'
+        rel = "guest"
         id_param = "name"
-    run = {'run': {
+    run = {"run": {
         "category": category_id,
         "level": level_id,
         "date": date,
@@ -167,7 +167,7 @@ def get_singlestar_run_body(user_id, category_id, run_time, date, platform_id, e
         }
     }}
     for var_id, var_value in stupid_vars.items():
-        run['run']['variables'][var_id] = var_value
+        run["run"]["variables"][var_id] = var_value
     return run
 
 
@@ -202,14 +202,14 @@ def request_src(url):
     if err_cnt >= err_lim:
         print(f"err_cnt greater than err_limit {err_cnt} >= {err_lim}")
         raise requests.exceptions.RequestException
-    # sleep so we don't get banned by requesting too much, .1,.2 give errors after doing a few hundred
+    # sleep so we don"t get banned by requesting too much, .1,.2 give errors after doing a few hundred
     time.sleep(_sleep_period)
     if resp is not None and (resp.status_code == 200 or resp.status_code == 201):
         return resp.json()
     elif resp.status_code != 200 and resp.status_code != 201:
         print(resp.text)
         if (resp.status_code == 400 and resp.json()[
-            'message'] == 'The selected category is for individual-level runs, but no level was selected.') \
+            "message"] == "The selected category is for individual-level runs, but no level was selected.") \
                 or resp.status_code == 404:
             return resp.json()
         else:
@@ -227,7 +227,7 @@ def post_src(url, body, api_key):
     request_finished = False
     resp = None
     new_headers = _h
-    new_headers['X-API-Key'] = api_key
+    new_headers["X-API-Key"] = api_key
     # print(f"attempting to request from:{url}")
     # usually fails if we are requesting too quickly, even with the sleep, try 10 times fail if still not working
     # after 10
@@ -250,7 +250,7 @@ def post_src(url, body, api_key):
     if err_cnt >= err_lim:
         print(f"err_cnt greater than err_limit {err_cnt} >= {err_lim}")
         raise requests.exceptions.RequestException
-    # sleep so we don't get banned by requesting too much, .1,.2 give errors after doing a few hundred
+    # sleep so we don"t get banned by requesting too much, .1,.2 give errors after doing a few hundred
     time.sleep(_sleep_period)
     if resp is not None and (resp.status_code == 200 or resp.status_code == 201):
         return resp.json()
@@ -268,7 +268,7 @@ def put_src(url, body, api_key):
     request_finished = False
     resp = None
     new_headers = _h
-    new_headers['X-API-Key'] = api_key
+    new_headers["X-API-Key"] = api_key
     # print(f"attempting to request from:{url}")
     # usually fails if we are requesting too quickly, even with the sleep, try 10 times fail if still not working
     # after 10
@@ -291,7 +291,7 @@ def put_src(url, body, api_key):
     if err_cnt >= err_lim:
         print(f"err_cnt greater than err_limit {err_cnt} >= {err_lim}")
         raise requests.exceptions.RequestException
-    # sleep so we don't get banned by requesting too much, .1,.2 give errors after doing a few hundred
+    # sleep so we don"t get banned by requesting too much, .1,.2 give errors after doing a few hundred
     time.sleep(_sleep_period)
     if resp is not None and (resp.status_code == 200 or resp.status_code == 201):
         return resp.json()
