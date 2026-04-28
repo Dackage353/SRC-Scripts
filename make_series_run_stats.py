2b8b6fe9808850990c943a486d5bc992fe1f74d2
Daniel <daniel.g.hackel@gmail.com>
2026-04-28 01:29:13 -0500

    fix file type

--- make_series_run_stats.py
+++ make_series_run_stats.py
@@ -14,9 +14,9 @@ if __name__ == '__main__':
     file_name = 'all_runs'
     file_helper.make_csv_file_from_data_frame(df, f'output/{file_name}.csv')
 
-    file_helper.make_single_sort_csv(df, f'output/{file_name} - runs per game.txt', 'game_id', 'game_name', reference.game_names, True)
-    file_helper.make_single_sort_csv(df, f'output/{file_name} - runs per player.txt', 'solo_player_id', 'solo_player_name', reference.user_names, True)
-    file_helper.make_single_sort_csv(df, f'output/{file_name} - runs per verifier.txt', 'verifier_id', 'verifier_name', reference.user_names, False)
+    file_helper.make_single_sort_csv(df, f'output/{file_name} - runs per game.csv', 'game_id', 'game_name', reference.game_names, True)
+    file_helper.make_single_sort_csv(df, f'output/{file_name} - runs per player.csv', 'solo_player_id', 'solo_player_name', reference.user_names, True)
+    file_helper.make_single_sort_csv(df, f'output/{file_name} - runs per verifier.csv', 'verifier_id', 'verifier_name', reference.user_names, False)
     
     #all_pairs = [(category.game_id, category.id) for category in categories]
     #file_helper.make_double_sort_csv(df, f'output/{file_name} - runs per category.csv', 'game_id', 'category_id', 'game_name', 'category_name', reference.game_names, reference.category_names, all_pairs)
