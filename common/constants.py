cbaaf119c35821a10f7a254bb36d9615a3e4793f
Daniel <daniel.g.hackel@gmail.com>
2026-05-07 12:22:53 -0500

    add single and stage rta category name constants

--- common/constants.py
+++ common/constants.py
@@ -4,3 +4,48 @@ SECONDARY_SERIES = 'r4x29lpn' #secondary sm64 romhacks
 STAR_REVENGE_GAME_LIST = [
 
 ]
+
+SINGLE_STAR_CATEGORIES = [
+    'single star',
+    'single stars',
+    'single burger',
+    'single shines',
+    'single chips',
+    'stars',
+    'star%',
+    'star %',
+    'star',
+    'star igt',
+    'star speedruns',
+    'rank speedruns',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    'star 1',
+    'star 2',
+    'star 3',
+    'star 4',
+    'star 5',
+    'star 6',
+    '100 coin',
+    '100 coin star',
+    'run event',
+    'jump event',
+    'shell ride event',
+    'wall-kick event',
+    'cannon event',
+    'challenger - bob-omb king',
+]
+STAGE_RTA_CATEGORIES = [
+    'stage rta',
+    'course rta',
+    '100%',
+    'stage rta (multi sc)',
+    'full level rta',
+    'individual levels',
+    'iw'
+]
