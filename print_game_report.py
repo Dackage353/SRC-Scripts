9a454ed971767030b283b9f522410caffe75ea94
Daniel <daniel.g.hackel@gmail.com>
2026-05-09 11:06:43 -0500

    full category printing in game report

--- print_game_report.py
+++ print_game_report.py
@@ -1,27 +1,53 @@
 from common import fetch_handler, file_helper, reference, src_helper, tool, constants
 from classes import Game
+import time
 
+
+script_start = time.perf_counter()
 
 FORCE_FETCH = False
 
 
 if __name__ == '__main__':
-    game_data = fetch_handler.get_series_info(constants.MAIN_SERIES, FORCE_FETCH)
-    game_data.extend(fetch_handler.get_series_info(constants.SECONDARY_SERIES, FORCE_FETCH))
+    with open("output.txt", "w", encoding="utf-8") as f:
+        game_data = fetch_handler.get_series_info(constants.MAIN_SERIES, FORCE_FETCH)
+        game_data.extend(fetch_handler.get_series_info(constants.SECONDARY_SERIES, FORCE_FETCH))
 
-    games = []
+        games = []
 
-    for data in game_data:
-        games.append(Game(data))
+        for data in game_data:
+            games.append(Game(data))
 
-    for game in games:
-        print(game.name)
-        print('-----')
+        fullgame_category_count = 0
+        level_count = 0
+        single_star_count = 0
 
-        for fullgame_category in game.fullgame_categories:
-            print(fullgame_category.name)
+        for game in games:
+            print(game.name, file=f)
+            print('-----', file=f)
 
-        print()
+            fullgame_category_count += len(game.fullgame_categories)
+            level_count += len(game.levels)
+            single_star_count += len(game.single_stars)
 
+            print('fullgames\n-', file=f)
+            for fullgame_category in game.fullgame_categories:
+                print(fullgame_category.name, file=f)
 
+            print('\nstage_rtas\n-', file=f)
+            for stage_rta in game.stage_rtas:
+                print(stage_rta.name, file=f)
 
+            print('\nsingle_stars\n-', file=f)
+            for single_star in game.single_stars:
+                print(single_star.name, file=f)
+
+            print('', file=f)
+
+        print(f'games: {len(games)}', file=f)
+        print(f'fullgame_categories: {fullgame_category_count}', file=f)
+        print(f'levels: {level_count}', file=f)
+        print(f'single_stars: {single_star_count}', file=f)
+    
+    script_end = time.perf_counter()
+    print(f"\nElapsed time: {script_end - script_start:.2f} seconds")
